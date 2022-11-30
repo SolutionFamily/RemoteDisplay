ed491ef3888d78acffcbeb2bd4c6558b0f453120
Chris Tacke <ctacke@opennetcf.com>
2022-11-30 11:23:12 -0600

    added DisplayBox

---
+++ MicroLayout/DisplayBox.cs
@@ -0,0 +1,28 @@
+﻿using Meadow.Foundation;
+using Meadow.Foundation.Graphics;
+
+namespace MicroLayout;
+
+public class DisplayBox : DisplayControl
+{
+    private Color _foreColor;
+
+    public DisplayBox(int left, int top, int width, int height, DisplayTheme? theme = null)
+        : base(left, top, width, height)
+    {
+    }
+
+    public Color ForeColor
+    {
+        get => _foreColor;
+        set => SetInvalidatingProperty(ref _foreColor, value);
+    }
+
+    protected override void OnDraw(MicroGraphics graphics)
+    {
+        if (ForeColor != Color.Transparent)
+        {
+            graphics.DrawRectangle(Left, Top, Width, Height, ForeColor, true);
+        }
+    }
+}
